9a2a89225efaf3458750832c20984fa418eb23e3
Samuel <samieoseh@gmail.com>
2025-10-05 09:28:50 +0100

    feat(cart): integrate safeRequest for API calls and improve error handling fix(cart): adjust font size for labelSmall in fonts configuration refactor(product): enhance ProductCard component layout and stock handling

--- apps/client/src/features/products/services/ProductsService.ts
+++ apps/client/src/features/products/services/ProductsService.ts
@@ -1,12 +1,12 @@
 import { PRODUCTS_ENDPOINTS } from "@/src/shared/constants";
-import { basicApiClient } from "@/src/shared/services";
+import { basicApiClient, safeRequest } from "@/src/shared/services";
 import { Product } from "../types/products";
 
 export class ProductsService {
     static async getAll() {
-       const results = await basicApiClient.get<{data:{products: Product[]}}>(
+       const results = await safeRequest(() => basicApiClient.get<{data:{products: Product[]}}>(
             PRODUCTS_ENDPOINTS.GET_ALL,
-        )
-        return results.data.products
+        ))
+        return results?.data.products
     }
 }
